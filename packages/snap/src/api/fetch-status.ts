b5c21762f159483fde1380b4418cca0eccf97bac
Stanley Yuen <102275989+stanleyyconsensys@users.noreply.github.com>
2026-04-21 20:58:52 +0800

    chore: remove unuse file

--- packages/snap/src/api/fetch-status.ts
+++
@@ -1,7 +0,0 @@
-export enum FetchStatus {
-  Initial = 'initial',
-  Fetching = 'fetching',
-  Fetched = 'fetched',
-  // eslint-disable-next-line @typescript-eslint/no-shadow
-  Error = 'error',
-}
