18bc08738903e3a34c22e8e2f5542c077a1653af
Stanley Yuen <102275989+stanleyyconsensys@users.noreply.github.com>
2026-04-29 13:44:01 +0800

    chore: remove unused file

--- packages/snap/src/utils/exceptions.ts
+++
@@ -1,9 +0,0 @@
-export class HttpException extends Error {
-  readonly status: number;
-
-  constructor(message: string, status: number) {
-    super(message);
-    this.name = 'HttpException';
-    this.status = status;
-  }
-}
