fe1b1c5de18c9453e149390ca5a08ec097d5139d
Stanley Yuen <102275989+stanleyyconsensys@users.noreply.github.com>
2026-08-14 22:07:53 +0800

    fix: remove unuse method

--- packages/snap/src/services/transaction/KeyringTransactionBuilder.ts
+++ packages/snap/src/services/transaction/KeyringTransactionBuilder.ts
@@ -4,7 +4,6 @@ import {
   TransactionType,
 } from '@metamask/keyring-api';
 import type { Transaction as KeyringTransaction } from '@metamask/keyring-api';
-import { hasProperty } from '@metamask/utils';
 
 import type { KnownCaip2ChainId } from '../../api';
 import type { KnownCaip19AssetIdOrSlip44Id } from '../../api/asset';
@@ -19,7 +18,6 @@ export enum KeyringTransactionType {
   ChangeTrustOptIn = 'changeTrustOptIn',
   ChangeTrustOptOut = 'changeTrustOptOut',
   Send = 'send',
-  Pending = 'pending',
   Unknown = 'unknown',
 }
 
@@ -68,27 +66,6 @@ export type ChangeTrustTransactionRequest = {
   fees?: KeyringTransaction['fees'];
 };
 
-export type PendingTransactionRequest = {
-  txId: string;
-  account: StellarKeyringAccount;
-  scope: KnownCaip2ChainId;
-  status?: TransactionStatus;
-} & (
-  | {
-      transactionType?: TransactionType;
-      asset: {
-        type: KnownCaip19AssetIdOrSlip44Id;
-        symbol: string;
-      };
-    }
-  | {
-      transactionType: TransactionType;
-      from: KeyringTransaction['from'];
-      to: KeyringTransaction['to'];
-      fees?: KeyringTransaction['fees'];
-    }
-);
-
 export type UnknownTransactionRequest = {
   txId: string;
   account: StellarKeyringAccount;
@@ -113,10 +90,6 @@ export type KeyringTransactionRequest =
   | {
       type: KeyringTransactionType.Send;
       request: SendTransactionRequest;
-    }
-  | {
-      type: KeyringTransactionType.Pending;
-      request: PendingTransactionRequest;
     }
   | {
       type: KeyringTransactionType.Unknown;
@@ -144,8 +117,6 @@ export class KeyringTransactionBuilder {
         return this.#createSendTransaction(request.request);
       case KeyringTransactionType.Swap:
         return this.#createSwapTransaction(request.request);
-      case KeyringTransactionType.Pending:
-        return this.#createPendingTransaction(request.request);
       case KeyringTransactionType.Unknown:
       case KeyringTransactionType.BridgeSend:
         return this.#createUnknownTransaction(request.request);
@@ -233,67 +204,6 @@ export class KeyringTransactionBuilder {
       status,
       timestamp,
       fees,
-    });
-  }
-
-  #createPendingTransaction(
-    request: PendingTransactionRequest,
-  ): KeyringTransaction {
-    const timestamp = this.getCreateTime();
-    const { txId, account, scope } = request;
-    const status = request.status ?? TransactionStatus.Unconfirmed;
-
-    // if the request has from and to, it is a pending classic swap transaction
-    if (hasProperty(request, 'from')) {
-      const swapRequest = request as Extract<
-        PendingTransactionRequest,
-        { from: KeyringTransaction['from'] }
-      >;
-      return this.#buildKeyringTransaction({
-        type: swapRequest.transactionType,
-        id: txId,
-        account,
-        scope,
-        from: swapRequest.from,
-        to: swapRequest.to,
-        status,
-        timestamp,
-        fees: swapRequest.fees ?? [],
-      });
-    }
-
-    const { asset } = request;
-
-    return this.#buildKeyringTransaction({
-      type: request.transactionType ?? TransactionType.Unknown,
-      id: txId,
-      account,
-      scope,
-      from: [
-        {
-          address: account.address,
-          asset: {
-            unit: asset.symbol,
-            type: asset.type,
-            amount: '0',
-            fungible: true,
-          },
-        },
-      ],
-      to: [
-        {
-          address: account.address,
-          asset: {
-            unit: asset.symbol,
-            type: asset.type,
-            amount: '0',
-            fungible: true,
-          },
-        },
-      ],
-      status,
-      timestamp,
-      fees: [],
     });
   }
 
